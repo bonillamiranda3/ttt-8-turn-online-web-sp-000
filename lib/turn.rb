c5bcd72b54702a08cf6ef921290145e6419a9936
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-19 03:06:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,16 +6,19 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 def valid_move?(board, index)
   if index.between?(0, 8)
     return true if board[index] == " "
   end
   return false
 end
+
 def input_to_index(input)
   int = input.to_i
   int - 1
 end
+
 def move(board, index, char = "X")
   board[index] = char
 end
